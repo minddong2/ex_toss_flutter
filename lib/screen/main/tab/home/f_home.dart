34b7c5ee76533f9da57a0de930934d4ec488cdd5
Minji <qnsghdharo119.dev@gmail.com>
2023-10-09 22:17:01 +0900

    notification create

---
+++ lib/screen/main/tab/home/f_home.dart
@@ -0,0 +1,63 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/round_button_theme.dart';
+import 'package:fast_app_base/common/widget/w_empty_expanded.dart';
+import 'package:fast_app_base/common/widget/w_round_button.dart';
+import 'package:fast_app_base/common/widget/w_rounded_container.dart';
+import 'package:fast_app_base/screen/dialog/d_message.dart';
+import 'package:fast_app_base/screen/main/tab/home/bank_accounts_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
+import 'package:fast_app_base/screen/main/tab/home/w_toss_app_bar.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../common/widget/w_big_button.dart';
+import '../../../dialog/d_color_bottom.dart';
+import '../../../dialog/d_confirm.dart';
+import '../../s_main.dart';
+
+class HomeFragment extends StatelessWidget {
+  const HomeFragment({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          RefreshIndicator(
+            edgeOffset: TossAppBar.appBarHeight,
+            onRefresh: () async{
+              await sleepAsync(500.ms as int);
+            },
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(
+                  top: TossAppBar.appBarHeight, bottom: MainScreenState.bottomNavigatorHeight),
+              child: Column(
+                children: [
+                  BigButton(
+                    "토스뱅크",
+                    onTap: () {
+                      context.showSnackbar("토스뱅크를 눌렀어요.test");
+                    },
+                  ),
+                  height10,
+                  RoundedContainer(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      "자산".text.bold.white.make(),
+                      height5,
+                      ...bankAccounts.map((e) => BankAccountWidget(e)).toList(),
+                    ],
+                  )),
+                ],
+              ).pSymmetric(h: 20),
+            ),
+          ),
+          const TossAppBar()
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/main/tab/tab_item.dart
@@ -0,0 +1,37 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/all/f_all.dart';
+import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
+import 'package:fast_app_base/screen/main/tab/stock/f_stock.dart';
+import 'package:fast_app_base/screen/main/tab/tosspay/f_tosspay.dart';
+import 'package:flutter/material.dart';
+import 'benefit/f_benefit.dart';
+
+enum TabItem {
+  home(Icons.home, '홈', HomeFragment()),
+  benefit(Icons.star, '혜택', BenefitFragement()),
+  tosspay(Icons.payment, '토스페이', TosspayFragment()),
+  stock(Icons.candlestick_chart, '주식', StockFragement()),
+  all(Icons.menu, '전체', AllFragment());
+
+  final IconData activeIcon;
+  final IconData inActiveIcon;
+  final String tabName;
+  final Widget firstPage;
+
+  const TabItem(this.activeIcon, this.tabName, this.firstPage,
+      {IconData? inActiveIcon})
+      : inActiveIcon = inActiveIcon ?? activeIcon;
+
+  BottomNavigationBarItem toNavigationBarItem(BuildContext context,
+      {required bool isActivated}) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          key: ValueKey(tabName),
+          isActivated ? activeIcon : inActiveIcon,
+          color: isActivated
+              ? context.appColors.iconButton
+              : context.appColors.iconButtonInactivate,
+        ),
+        label: tabName);
+  }
+}
